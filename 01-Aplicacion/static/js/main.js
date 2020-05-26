a94782daa283e9f4182646d0d0189a0b4e725598
Mario Torres <48029021+marito07@users.noreply.github.com>
2020-05-26 19:53:23 +0200

    Algoritmo acabado

--- 01-Aplicacion/static/js/main.js
+++ 01-Aplicacion/static/js/main.js
@@ -59,12 +59,12 @@ $(function () {
     /* Muestra la tabla inicial de ejemplos en la ventana -----------------------------*/
     let htmlTxt = '<thead><tr>';
     for (let i = 0; i < listaDeAtributos.length; i++) {
-      htmlTxt +='<th>' + listaDeAtributos[i] + '</th>';
+      htmlTxt += '<th>' + listaDeAtributos[i] + '</th>';
     }
     htmlTxt += '</tr></thead><tbody>';
-    for(let i = 0; i < listaDeEjemplos.length; i++){
+    for (let i = 0; i < listaDeEjemplos.length; i++) {
       htmlTxt += '<tr>';
-      for(let j = 0; j < listaDeAtributos.length; j++){
+      for (let j = 0; j < listaDeAtributos.length; j++) {
         htmlTxt += '<td>' + listaDeEjemplos[i][listaDeAtributos[j]] + '</td>';
       }
       htmlTxt += '</tr>';
@@ -76,70 +76,146 @@ $(function () {
 
     $('#aplicationStart').prop('disabled', true); // Desactiva el boton de inicio de la aplicación
     var atributoResultado = listaDeAtributos[listaDeAtributos.length - 1]; // Atributo que indica "si" o "no"
-    console.log('El atributo resultado es: ');
+    console.log('El atributo donde aparece el resultado es: ');
     console.log(atributoResultado);
-
-
-    // Carga los valores de "si" y "no" cada atributo ---------------------------------------------------------
-    var salida = {};
-    for (var aux = 0; aux < listaDeAtributos.length - 1; aux++) {
-      salida[listaDeAtributos[aux]] = {};
-    }
-    for (var cont = 0; cont < listaDeEjemplos.length; cont++) { // Carga los valores de "si" y "no" cada atributo
-      var stringEje = "" + listaDeEjemplos[cont][atributoResultado];
-      if (stringEje.trim() == "si") {
-        for (var cont2 = 0; cont2 < listaDeAtributos.length - 1; cont2++) {
-          var helpString = "" + listaDeEjemplos[cont][listaDeAtributos[cont2]];
-          if (!salida[listaDeAtributos[cont2]].hasOwnProperty(helpString)) {
-            salida[listaDeAtributos[cont2]][helpString] = {};
-            salida[listaDeAtributos[cont2]][helpString].pData = 1;
-            salida[listaDeAtributos[cont2]][helpString].nData = 0;
-            salida[listaDeAtributos[cont2]][helpString].atrib = listaDeAtributos[cont2];
-          }
-          else if (salida[listaDeAtributos[cont2]][helpString].pData == undefined) {
-            salida[listaDeAtributos[cont2]][helpString].pData = 1;
-          }
-          else {
-            salida[listaDeAtributos[cont2]][helpString].pData++;
-          }
-        }
-      }
-      else {
-        for (var cont2 = 0; cont2 < listaDeAtributos.length - 1; cont2++) {
-          var helpString = "" + listaDeEjemplos[cont][listaDeAtributos[cont2]];
-          if (!salida[listaDeAtributos[cont2]].hasOwnProperty(helpString)) {
-            salida[listaDeAtributos[cont2]][helpString] = {};
-            salida[listaDeAtributos[cont2]][helpString].nData = 1;
-            salida[listaDeAtributos[cont2]][helpString].pData = 0;
-            salida[listaDeAtributos[cont2]][helpString].atrib = listaDeAtributos[cont2];
-          }
-          else if (salida[listaDeAtributos[cont2]][helpString].nData == undefined) {
-            salida[listaDeAtributos[cont2]][helpString].nData = 1;
-          }
-          else {
-            salida[listaDeAtributos[cont2]][helpString].nData++;
-          }
-        }
-      }
-    }
-    // ---------------------------------------------------------------------------------------
-    // --------- Carga los valores de p y n para cada valor de los atributos -----------------
-    var keys = Object.keys(salida);
-    for (let i = 0; i < keys.length; i++) {
-      var keys2 = Object.keys(salida[keys[i]]);
-      for (let j = 0; j < keys2.length; j++) {
-        salida[keys[i]][keys2[j]].p = salida[keys[i]][keys2[j]].pData / (salida[keys[i]][keys2[j]].pData + salida[keys[i]][keys2[j]].nData);
-        salida[keys[i]][keys2[j]].n = salida[keys[i]][keys2[j]].nData / (salida[keys[i]][keys2[j]].pData + salida[keys[i]][keys2[j]].nData);
-      }
-    }
-    // ---------------------------------------------------------------------------------------
-    console.log('--- Valores de los atributos p y n ----')
-    console.log(salida); 
-
+    id3(listaDeEjemplos, listaDeAtributos, atributoResultado, ''); // Llama a la función id3() donde se encuentra la implementación del algoritmo
   });
 
 
-  function id3() {
+  function id3(listEjem, listAtrib, atributoResultado, caracteres) { // Implementación del algoritmo recursivo
+    // Carga los valores de "si" y "no" cada atributo ---------------------------------------------------------
+    if (listAtrib.length > 0) { // Caso base: Si el array tiene más de un elemento entra, si no no
+
+      let sonIgualesEnSigno = true;
+      let before = listEjem[0][atributoResultado];
+      let i = 1;
+      while (sonIgualesEnSigno && i < listEjem.length) {
+        if (before !== listEjem[i][atributoResultado]) {
+          sonIgualesEnSigno = false;
+        }
+        else {
+          i++;
+        }
+      }
+      if (sonIgualesEnSigno) {
+        let color = "green";
+        if(before.trim() == "no"){
+          color = "red";
+        }
+        $('#sol').append('<div style="color: ' + color + '">' + caracteres + "---> (" + before.trim() + ")</div>");
+      }
+      else {
+        var salida = {};
+        for (var aux = 0; aux < listAtrib.length - 1; aux++) {
+          salida[listAtrib[aux]] = {};
+        }
+        for (var cont = 0; cont < listEjem.length; cont++) { // Carga los valores de "si" y "no" cada atributo
+          var stringEje = "" + listEjem[cont][atributoResultado];
+          if (stringEje.trim() == "si") {
+            for (var cont2 = 0; cont2 < listAtrib.length - 1; cont2++) {
+              var helpString = "" + listEjem[cont][listAtrib[cont2]];
+              if (!salida[listAtrib[cont2]].hasOwnProperty(helpString)) {
+                salida[listAtrib[cont2]][helpString] = {};
+                salida[listAtrib[cont2]][helpString].pData = 1;
+                salida[listAtrib[cont2]][helpString].nData = 0;
+              }
+              else if (salida[listAtrib[cont2]][helpString].pData == undefined) {
+                salida[listAtrib[cont2]][helpString].pData = 1;
+              }
+              else {
+                salida[listAtrib[cont2]][helpString].pData++;
+              }
+            }
+          }
+          else {
+            for (var cont2 = 0; cont2 < listAtrib.length - 1; cont2++) {
+              var helpString = "" + listEjem[cont][listAtrib[cont2]];
+              if (!salida[listAtrib[cont2]].hasOwnProperty(helpString)) {
+                salida[listAtrib[cont2]][helpString] = {};
+                salida[listAtrib[cont2]][helpString].nData = 1;
+                salida[listAtrib[cont2]][helpString].pData = 0;
+              }
+              else if (salida[listAtrib[cont2]][helpString].nData == undefined) {
+                salida[listAtrib[cont2]][helpString].nData = 1;
+              }
+              else {
+                salida[listAtrib[cont2]][helpString].nData++;
+              }
+            }
+          }
+        }
+        // ---------------------------------------------------------------------------------------
+        // --------- Carga los valores de p,a,n y N para cada valor de los atributos -----------------
+        var keys = Object.keys(salida); // Valores de los atributos
+        for (let i = 0; i < keys.length; i++) {
+          var keys2 = Object.keys(salida[keys[i]]);
+          salida[keys[i]].N = 0;
+          for (let j = 0; j < keys2.length; j++) {
+            salida[keys[i]][keys2[j]].a = salida[keys[i]][keys2[j]].pData + salida[keys[i]][keys2[j]].nData;
+            salida[keys[i]][keys2[j]].p = salida[keys[i]][keys2[j]].pData / (salida[keys[i]][keys2[j]].pData + salida[keys[i]][keys2[j]].nData);
+            salida[keys[i]][keys2[j]].n = salida[keys[i]][keys2[j]].nData / (salida[keys[i]][keys2[j]].pData + salida[keys[i]][keys2[j]].nData);
+            salida[keys[i]].N += salida[keys[i]][keys2[j]].pData + salida[keys[i]][keys2[j]].nData;
+          }
+          salida[keys[i]].merito = 0; // Se inicializa el merito del atributo a 0
+          for (let j = 0; j < keys2.length; j++) {
+            salida[keys[i]][keys2[j]].r = salida[keys[i]][keys2[j]].a / salida[keys[i]].N; // Calculo del valor de r
+            salida[keys[i]][keys2[j]].infor = infor(salida[keys[i]][keys2[j]].p, salida[keys[i]][keys2[j]].n); // Calculo de la entropia de ese valor
+            salida[keys[i]].merito += (salida[keys[i]][keys2[j]].r * salida[keys[i]][keys2[j]].infor); // Se suman los sucesivos resultados de los valores del atributo al merito total.
+          }
+        }
+        // ---------------------------------------------------------------------------------------
+        console.log('--- Valores de los atributos pi,ni,ai, y N ----')
+        console.log(salida);
+
+        // Seleccionar al atributo con el merito mas bajo ----------------------------------------
+        let AtributoSeleccionado = keys[0];
+        let valormenor = salida[keys[0]].merito;
+        for (let i = 1; i < keys.length; i++) {
+          if (salida[keys[i]].merito < valormenor) {
+            valormenor = salida[keys[i]].merito;
+            AtributoSeleccionado = keys[i];
+          }
+        }
+        console.log("El atributo seleccionado es: " + AtributoSeleccionado);
+        $('#sol').append('<div style="color: blue;">' + caracteres + "Nodo: " + AtributoSeleccionado + '</div>');
+        var ramas = Object.keys(salida[AtributoSeleccionado]);
+        for (let i = 0; i < ramas.length; i++) {
+          if (ramas[i] !== 'N' && ramas[i] !== 'merito') {
+            $('#sol').append('<div style="color: purple;">' + caracteres + "= Rama: " + ramas[i] + '</div>');
+            let nuevaListaEje = [];
+            let nuevaListaAtrib = [];
+
+            for(let q = 0; q < listEjem.length; q++){
+              if(listEjem[q][AtributoSeleccionado] === ramas[i]){
+                nuevaListaEje.push(listEjem[q]);
+              }
+            }
+            for(let q = 0; q < listAtrib.length; q++){ 
+              if(listAtrib[q] !== AtributoSeleccionado){
+                nuevaListaAtrib.push(listAtrib[q]);
+              }
+            }
+            id3(nuevaListaEje, nuevaListaAtrib, atributoResultado, caracteres + '--');
+          }
+        }
+      }
+
+
+      // ---------------------------------------------------------------------------------------
+      /*     let htmlTexto = '<h3>Tabla de méritos de los atributos</h3><table class ="table" style="border: 2px solid black;">';
+          htmlTexto += '<thead><tr>';
+          for(let i = 0; i < listAtrib.length - 1; i++) {
+            htmlTexto += '<th>' + listAtrib[i] + '</th>';
+          }
+          htmlTexto += '</tr></thead>';
+          htmlTexto += '<tbody><tr>';
+          for(let i = 0; i < listAtrib.length - 1; i++) {
+            htmlTexto += '<td>' + salida[keys[i]].merito + '</td>';
+          }
+      
+          htmlTexto += '</tr></tbody></table>';
+          $('#sol').append(htmlTexto); */
+    }
 
   }
 
